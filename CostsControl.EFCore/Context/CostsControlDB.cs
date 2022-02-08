cc87bbc73e997e38399e2db1e8776a5037b7ea3e
Oleg Smolin <olegsmolin2000@mail.ru>
2022-02-09 01:50:57 +0300

    connect mysql

---
+++ CostsControl.EFCore/Context/CostsControlDB.cs
@@ -0,0 +1,17 @@
+﻿using CostsControl.EFCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CostsControl.EFCore.Context
+{
+    public class CostsControlDB:DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;database=TestDB;user=Oleg;password=Zhopa123456");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ CostsControl.EFCore/Entities/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace CostsControl.EFCore.Entities
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Amount { get; set; }
+    }
+}
